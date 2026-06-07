0768475ea5438b9c9af01db97e30d6c865c1bd29
Damian Wujec <wujecdamian12@gmail.com>
2026-06-07 19:10:07 +0200

    Initialize Express application with routing and EJS view engine

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//routes
+app.get("/", (req, res) => {
+  console.log("usernames will be logged here - wip");
+});
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+app.post("/new", (req, res) => {
+  console.log("username to be saved: ", req.body.username);
+});
+
+//listen
+app.listen(3000, () => {
+  console.log("App listening on port http://localhost:3000");
+});
