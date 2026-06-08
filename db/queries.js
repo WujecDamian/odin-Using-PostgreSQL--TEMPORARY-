4c87cd8eef4998bd8d0278854fd63ba6065515f4
Damian Wujec <wujecdamian12@gmail.com>
2026-06-08 23:25:35 +0200

    feat: add searchUsername function to queries.js for username search functionality

--- db/queries.js
+++ db/queries.js
@@ -9,7 +9,14 @@ async function insertUsername(usernames) {
   await pool.query("INSERT INTO usernames (username) VALUES ($1)", [usernames]);
 }
 
+async function searchUsername(searchQuery) {
+  await pool.query("SELECT * FROM usernames WHERE username LIKE (%$1%)", [
+    searchQuery,
+  ]);
+}
+
 module.exports = {
   getAllUsernames,
   insertUsername,
+  searchUsername,
 };
